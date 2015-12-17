02bcaa82f7f01cfa5a7b01113aa9550b2b8624db
高亚斌 <gaoyabing@126.com>
2015-12-17 15:45:51 +0800

    AddCode
    添加代码

---
+++ 灵读/frm灵读.Designer.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace 灵读
+{
+    partial class frm灵读
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.rtb文本框 = new System.Windows.Forms.RichTextBox();
+            this.pan容器 = new System.Windows.Forms.Panel();
+            this.lab阅读时长 = new System.Windows.Forms.Label();
+            this.lab时间 = new System.Windows.Forms.Label();
+            this.lkl全屏否 = new System.Windows.Forms.LinkLabel();
+            this.tim计时器 = new System.Windows.Forms.Timer(this.components);
+            this.pan容器.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rtb文本框
+            // 
+            this.rtb文本框.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtb文本框.BackColor = System.Drawing.Color.Black;
+            this.rtb文本框.EnableAutoDragDrop = true;
+            this.rtb文本框.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.rtb文本框.ForeColor = System.Drawing.Color.White;
+            this.rtb文本框.Location = new System.Drawing.Point(0, 36);
+            this.rtb文本框.Name = "rtb文本框";
+            this.rtb文本框.Size = new System.Drawing.Size(800, 600);
+            this.rtb文本框.TabIndex = 0;
+            this.rtb文本框.Text = "直接拖入文本即可\n可以做编辑器使用,但不建议\n的\n有\n是";
+            // 
+            // pan容器
+            // 
+            this.pan容器.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pan容器.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pan容器.Controls.Add(this.lab阅读时长);
+            this.pan容器.Controls.Add(this.lab时间);
+            this.pan容器.Controls.Add(this.lkl全屏否);
+            this.pan容器.Location = new System.Drawing.Point(0, 0);
+            this.pan容器.Name = "pan容器";
+            this.pan容器.Size = new System.Drawing.Size(802, 30);
+            this.pan容器.TabIndex = 1;
+            // 
+            // lab阅读时长
+            // 
+            this.lab阅读时长.AutoSize = true;
+            this.lab阅读时长.Location = new System.Drawing.Point(584, 7);
+            this.lab阅读时长.Name = "lab阅读时长";
+            this.lab阅读时长.Size = new System.Drawing.Size(59, 12);
+            this.lab阅读时长.TabIndex = 5;
+            this.lab阅读时长.Text = "阅读时长:";
+            // 
+            // lab时间
+            // 
+            this.lab时间.AutoSize = true;
+            this.lab时间.Location = new System.Drawing.Point(10, 7);
+            this.lab时间.Name = "lab时间";
+            this.lab时间.Size = new System.Drawing.Size(59, 12);
+            this.lab时间.TabIndex = 4;
+            this.lab时间.Text = "当前时间:";
+            // 
+            // lkl全屏否
+            // 
+            this.lkl全屏否.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.lkl全屏否.AutoSize = true;
+            this.lkl全屏否.Location = new System.Drawing.Point(742, 7);
+            this.lkl全屏否.Name = "lkl全屏否";
+            this.lkl全屏否.Size = new System.Drawing.Size(53, 12);
+            this.lkl全屏否.TabIndex = 3;
+            this.lkl全屏否.TabStop = true;
+            this.lkl全屏否.Text = "开始阅读";
+            this.lkl全屏否.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lkl全屏否_LinkClicked);
+            // 
+            // tim计时器
+            // 
+            this.tim计时器.Enabled = true;
+            this.tim计时器.Interval = 1000;
+            this.tim计时器.Tick += new System.EventHandler(this.tim计时器_Tick);
+            // 
+            // frm灵读
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 616);
+            this.Controls.Add(this.pan容器);
+            this.Controls.Add(this.rtb文本框);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frm灵读";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "灵读";
+            this.pan容器.ResumeLayout(false);
+            this.pan容器.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtb文本框;
+        private System.Windows.Forms.Panel pan容器;
+        private System.Windows.Forms.Label lab阅读时长;
+        private System.Windows.Forms.Label lab时间;
+        private System.Windows.Forms.LinkLabel lkl全屏否;
+        private System.Windows.Forms.Timer tim计时器;
+
+    }
+}
+
---
+++ 灵读/frm灵读.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 灵读
+{
+    public partial class frm灵读 : Form
+    {
+       private TimeSpan tms启动时间 = new TimeSpan(0);
+        private TimeSpan tms一秒 = new TimeSpan(10000000);
+       public frm灵读()
+        {
+            InitializeComponent();
+            tim计时器.Start();
+            lab时间.Text = "当前时间:" + string.Format("{0:T}", DateTime.Now) + "," + DateTime.Now.GetDateTimeFormats('D')[1] + ",第" + GetWeekOfYear() + "周" + ",第" + DateTime.Today.DayOfYear + "天";
+            lab阅读时长.Text = "阅读时长:" + tms启动时间;
+            rtb文本框.DragEnter += new DragEventHandler(rtb文本框_DragEnter);
+            rtb文本框.DragDrop += new DragEventHandler(rtb文本框_DragDrop);
+        }
+
+        private void rtb文本框_DragDrop(object sender, DragEventArgs e)
+        {
+            Array arrayFileName = (Array)e.Data.GetData(DataFormats.FileDrop);
+
+            string strFileName = arrayFileName.GetValue(0).ToString();
+            
+            StreamReader sr = new StreamReader(strFileName, TxtFileEncoder.GetEncoding(strFileName));
+            rtb文本框.Text = sr.ReadToEnd();
+            sr.Close();
+        }
+
+        private void rtb文本框_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Link;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private static int GetWeekOfYear()
+        {
+            int firstWeekend = 7 - Convert.ToInt32(DateTime.Parse(DateTime.Today.Year + "-1-1").DayOfWeek);//一.找到第一周的最后一天（先获取1月1日是星期几，从而得知第一周周末是几）
+            int currentDay = DateTime.Today.DayOfYear;//二.获取今天是一年当中的第几天
+            return Convert.ToInt32(Math.Ceiling((currentDay - firstWeekend) / 7.0)) + 1;//三.（今天 减去 第一周周末）/7 等于 距第一周有多少周 再加上第一周的1 就是今天是今年的第几周了刚好考虑了惟一的特殊情况就是，今天刚好在第一周内，那么距第一周就是0 再加上第一周的1 最后还是1
+        }
+        private void 全屏化()
+        {
+            this.TopMost = true;
+            this.WindowState = FormWindowState.Maximized;
+            this.FormBorderStyle = FormBorderStyle.None;
+            HighLightText();
+        }
+        private void 小窗口化()
+        {
+            this.TopMost = false;
+            this.WindowState = FormWindowState.Normal;
+            this.FormBorderStyle = FormBorderStyle.FixedToolWindow;
+        }
+        private void lkl全屏否_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            switch (this.FormBorderStyle)
+            {
+                case FormBorderStyle.None:
+                    小窗口化(); break;
+                case FormBorderStyle.FixedSingle:
+                    全屏化(); break;
+                case FormBorderStyle.Fixed3D:
+                    全屏化(); break;
+                case FormBorderStyle.FixedDialog:
+                    全屏化(); break;
+                case FormBorderStyle.Sizable:
+                    全屏化(); break;
+                case FormBorderStyle.FixedToolWindow:
+                    全屏化(); break;
+                case FormBorderStyle.SizableToolWindow:
+                    全屏化(); break;
+                default:
+                    全屏化(); break;
+            }
+        }
+
+        private void tim计时器_Tick(object sender, EventArgs e)
+        {
+            lab时间.Text = "当前时间:" + string.Format("{0:T}", DateTime.Now) + "," + DateTime.Now.GetDateTimeFormats('D')[1] + ",第" + GetWeekOfYear() + "周"+",第"+ DateTime.Today.DayOfYear + "天";
+            tms启动时间 = tms启动时间 + tms一秒;
+            lab阅读时长.Text = "阅读时长:" + tms启动时间;
+        }
+        private void HighLightText()
+        {	
+			string[] 动词 = { "有", "是", "你", "的", "我", "by", "group", "null" };
+            string[] 名词 = { "isnull", "count", "sum" };
+            string[] 标点 = { @"'((.|\n)*?)'" };
+			
+            string[] keywords = { "有", "是", "你", "的", "我", "by", "group", "null" };
+            string[] functions = { "isnull", "count", "sum" };
+            string[] strings = { @"'((.|\n)*?)'" };
+            string[] whiteSpace = { "\t", "\n", "   " };
+
+            rtb文本框.SelectAll();
+            rtb文本框.SelectionColor = Color.White;
+            HighLightText(keywords, Color.Red);//FromArgb(100,100,0));//Magenta
+            HighLightText(functions, Color.Red);
+            HighLightText(strings, Color.Gray);
+            HighLightText(whiteSpace, Color.Black);
+        }
+        private void HighLightText(string[] wordList, Color color)
+        {
+            foreach (string word in wordList)
+            {
+                Regex r = new Regex(word, RegexOptions.IgnoreCase);
+
+                foreach (Match m in r.Matches(rtb文本框.Text))
+                {
+                    rtb文本框.Select(m.Index, m.Length);
+                    rtb文本框.SelectionColor = color;
+                }
+            }
+        }
+    }
+}
